a3195ed0b36a0d8f70f82c5b0fa2fc5a0c0bf7f9
abutalha-97 <abutalhapappu@gmail.com>
2021-08-18 15:48:07 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,50 @@
+function productNumber(product,price,isIncreasing){
+    const productInput = document.getElementById(product + '-number');
+    let productNumber = productInput.value;
+    if(isIncreasing){
+        productNumber = parseInt(productNumber) + 1;
+    }
+    else if(productNumber > 0){
+        productNumber = parseInt(productNumber) - 1;
+    }
+    productInput.value = productNumber;
+    //update total
+    const productTotal =  document.getElementById(product+'-total'); 
+    productTotal.innerText = productNumber * price;
+    //Calculate Total
+    calculateTotal();   
+}
+
+function getInputValue(product){
+    const productInput = document.getElementById(product+'-number');
+    const productNumber = parseInt(productInput.value);
+    return productNumber;
+}
+
+function calculateTotal(){
+    const phoneTotal = getInputValue('phone') * 1219;
+    const caseTotal = getInputValue('case') * 59;
+    const subTotal = phoneTotal + caseTotal;
+    const tax = subTotal / 10;
+    const totalPrice = subTotal + tax;
+    //Update on the HTML
+    document.getElementById('sub-total').innerText = subTotal;
+    document.getElementById('tax-amount').innerText = tax;
+    document.getElementById('total-price').innerText = totalPrice;
+}
+
+
+//Handle case Increase Decrease Events For Phone
+document.getElementById('phone-plus').addEventListener('click',function(){
+    productNumber('phone',1219,true);
+ })
+ document.getElementById('phone-minus').addEventListener('click',function(){
+     productNumber('phone',1219,false);
+ })
+//Handle case Increase Decrease Events For Caseing
+document.getElementById('case-plus').addEventListener('click',function(){
+   productNumber('case',59,true);
+})
+document.getElementById('case-minus').addEventListener('click',function(){
+    productNumber('case',59,false);
+})
